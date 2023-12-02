fd18dace84e68d73a986418b5912010d6d671244
Rob Anderson <rob.anderson@sds.io>
2023-12-02 16:59:43 -0500

    Day 2.a puzzle

--- puzzles/day-2/day-2-a.ts
+++ puzzles/day-2/day-2-a.ts
@@ -1,10 +1,42 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from "../../shared.ts";
+import chalk from "chalk";
+
+const cubeBag = { red: 12, green: 13, blue: 14 };
 
 export async function day2a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let value = 0;
+
+  const textValues = JSON.stringify(data)
+    .split(`"`)
+    .filter(text => text.includes("Game"));
+
+  textValues.forEach(textLine => {
+    const gameValues = textLine.split(":");
+    const isPossible = checkPossibility(gameValues[1]);
+
+    if (isPossible) {
+      value += parseInt(gameValues[0].replace("Game ", ""));
+    }
+  });
+  return value;
 }
 
+function checkPossibility(cubeString: string): boolean {
+  const sets = cubeString.replace(/ /g, "").split(/[;,]/);
+
+  return sets.every(evaluateCubes);
+}
+
+const evaluateCubes = (cube: string): boolean => {
+  if (cube.includes("red")) {
+    return cubeBag.red >= parseInt(cube.replace("red", ""));
+  } else if (cube.includes("green")) {
+    return cubeBag.green >= parseInt(cube.replace("green", ""));
+  } else if (cube.includes("blue")) {
+    return cubeBag.blue >= parseInt(cube.replace("blue", ""));
+  }
+};
+
 const answer = await day2a();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.log(chalk.bgGreen("Your Answer:"), chalk.green(answer));
