0815d8fe344fa4376016b884f338a60030d79369
Rob Anderson <rob.anderson@sds.io>
2023-12-02 09:15:23 -0500

    Day 1.a puzzle

--- puzzles/day-1/day-1-a.ts
+++ puzzles/day-1/day-1-a.ts
@@ -1,10 +1,17 @@
-import { readData } from '../../shared.ts';
-import chalk from 'chalk';
+import { readData } from "../../shared.ts";
+import chalk from "chalk";
 
 export async function day1a(dataPath?: string) {
   const data = await readData(dataPath);
-  return 0;
+  let value = 0;
+
+  const textValues = JSON.stringify(data).split(",");
+  textValues.forEach(textLine => {
+    const numbers = textLine.replace(/[^0-9]/g, "");
+    value += parseInt(numbers[0] + numbers[numbers.length - 1]);
+  });
+  return value;
 }
 
 const answer = await day1a();
-console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
+console.log(chalk.bgGreen("Your Answer:"), chalk.green(answer));
