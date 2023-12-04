bda76f98c55c07b23231fe25c11782c692e30963
Rob Anderson <rob.anderson@sds.io>
2023-12-04 01:44:24 -0500

    Day 4.a puzzle refactor

--- puzzles/day-4/day-4-a.ts
+++ puzzles/day-4/day-4-a.ts
@@ -2,8 +2,30 @@ import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
 export async function day4a(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data: string[] = await readData(dataPath);
+  let value = 0;
+
+  const cardData = data.map(gameString => gameString.split(':')[1]);
+  cardData.forEach(game => (value += getGameValue(game)));
+  return value;
+}
+
+function getGameValue(game: string): number {
+  let value = 0;
+  const gameValues = game.split('|');
+  const winningNumbers = gameValues[0]
+    .trim()
+    .split(' ')
+    .filter(value => !!value);
+  const gameNumbers = gameValues[1]
+    .trim()
+    .split(' ')
+    .filter(value => !!value);
+
+  winningNumbers.forEach(number => {
+    if (gameNumbers.includes(number)) value = !value ? 1 : value * 2;
+  });
+  return value;
 }
 
 const answer = await day4a();
