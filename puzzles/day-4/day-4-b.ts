24aa4eefd6cdf8197f2100480670e78199e515e0
Rob Anderson <rob.anderson@sds.io>
2023-12-05 01:10:05 -0500

    Day 4.b puzzle

--- puzzles/day-4/day-4-b.ts
+++ puzzles/day-4/day-4-b.ts
@@ -1,10 +1,40 @@
 import { readData } from '../../shared.ts';
 import chalk from 'chalk';
 
+const totalCards: number[] = [];
+
 export async function day4b(dataPath?: string) {
-  const data = await readData(dataPath);
-  return 0;
+  const data: string[] = await readData(dataPath);
+  let value = 0;
+
+  const cardData = data.map(gameString => gameString.split(':')[1]);
+  cardData.forEach(() => totalCards.push(1));
+  cardData.forEach((game, index) => getScratchCards(game, index));
+  value = addScratchCards();
+  return value;
 }
+
+function getScratchCards(game: string, index: number): void {
+  let value = 0;
+  const gameValues = game.split('|');
+  const winningNumbers = gameValues[0]
+    .trim()
+    .split(' ')
+    .filter(value => !!value);
+  const gameNumbers = gameValues[1]
+    .trim()
+    .split(' ')
+    .filter(value => !!value);
+
+  winningNumbers.forEach(number => {
+    if (gameNumbers.includes(number)) value++;
+  });
+  for (let i = index + 1; i < index + value + 1; ++i) {
+    totalCards[i] += totalCards[index];
+  }
+}
+
+const addScratchCards = (): number => totalCards.reduce((a, b) => a + b, 0);
 
 const answer = await day4b();
 console.log(chalk.bgGreen('Your Answer:'), chalk.green(answer));
